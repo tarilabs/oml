9e75f3eaf1fb6da7daae7295d776a636785c9e34
tarilabs <matteo.mortari@gmail.com>
2024-07-01 08:06:57 +0200

    improve script

--- enable-dashboard.sh
+++ enable-dashboard.sh
@@ -3,6 +3,7 @@
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 
+echo "Waiting for all Deployments in kubernetes-dashboard..."
 DEPLOYMENTS=$(kubectl get deployments -n kubernetes-dashboard -o jsonpath='{.items[*].metadata.name}')
 for DEPLOYMENT in $DEPLOYMENTS; do # bash separation
   kubectl wait --for=condition=available --timeout=2m deployment/$DEPLOYMENT -n kubernetes-dashboard
@@ -15,5 +16,8 @@ sleep 2
 kubectl apply -f dashboard/bearerTokenSecret.yaml
 sleep 3
 
+echo "Bearer token:"
 kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d
+echo ""
+
 kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
